32ea97567a74e72a7955a1350a58094961110a48
Jennifer Liu <jenniferliu1272@gmail.com>
2023-05-07 15:00:54 -0700

    Add endpoint to handle webhook payload.

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -27,6 +28,20 @@ func HelloHandler(w http.ResponseWriter, r *http.Request) {
     w.Write([]byte("Hello, Web!\n"))
 }
 
+func PayloadHandler(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Reading payload!\n"))
+    // read the body of request
+    var bodyBytes []byte
+    if r.Body != nil {
+        bodyBytes, _ = ioutil.ReadAll(r.Body)
+    }
+    // write bodyBytes to file
+    err := ioutil.WriteFile("payload.txt", bodyBytes, 0644)
+    if err != nil {
+        log.Fatal(err)
+    }
+}
+
 func main() {
 
     //get the value of the ADDR environment variable
@@ -46,6 +61,7 @@ func main() {
     //tell it to call the HelloHandler() function
     //when someone requests the resource path `/hello`
     mux.HandleFunc("/", HelloHandler)
+    mux.HandleFunc("/payload", PayloadHandler)
 
     //start the web server using the mux as the root handler,
     //and report any errors that occur.
